8288de2d444212d0224a065eb775c8df49edee44
Luis Lavaire <llavaire01@gmail.com>
2020-10-28 13:41:15 -0600

    added download URL.

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
 
-set -e -- "{$1:-tbd}"
+#   get it from: https://git.io/JT11Y:
+#   curl -L https://git.io/JT11Y
+
+set -e -- "${1:-tbd}"
 
 curl -L "https://raw.githubusercontent.com/luis-lavaire/tbd/main/tbd" > "$1"
 chmod +x "$1"
